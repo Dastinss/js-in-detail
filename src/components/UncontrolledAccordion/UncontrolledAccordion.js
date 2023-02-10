698d24df6b0d87cdf4fb819fe92e6ced0bb04034
Dastinss <sergeyfdaragan@gmail.com>
2023-02-11 00:43:11 +0200

    Lesson #16 13:48

--- src/components/UncontrolledAccordion/UncontrolledAccordion.js
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.js
@@ -1,15 +1,25 @@
-import React, {useState} from "react";
+import React, { useReducer, useState } from "react";
 
 type AccordionPropsType = {
     titleValue: string,
     // collapsed: boolean
 }
 
+type ActionType = {
+    type: string // action это объект у которого обязателно должно быть св=во тип (надпись "удалить", "уволить", "выучить")
+}
+
+const reducer = (state: boolean, action) => { // ф=ция в которую поступает state (переключалка) и action (инструкция, у которой должен біть тип\заголовок, что должен сделать редюсер) и которая по каким то правилам меняет стейт и его в конечном итоге выплевывает изменнній стейт
+
+    return state;
+}
+
 export function UncontrolledAccordion(props: AccordionPropsType) {
     console.log('UnAccordion rendering')
 
     // const collapsed = true;
-    let [collapsed, setCollapsed]=useState(false)
+    // let [collapsed, setCollapsed]=useState(false) // закоментили в уроке 16 ,когда ввели useReducer
+    let [collapsed, dispatch]=useReducer(reducer, false) //говорим -пользуйся reducer_ом, а вторыи параметрои передаем начальное значение (false). получаем значение ... и ф-ция ,которая позволит отправлять в мир реакта инструкцию в редьюсер єти єкщены
 
     return <div>
         <AccordionTitle title={props.titleValue} onClick={ () => {setCollapsed(!collapsed) } }/>
